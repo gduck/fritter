35c85782b4e5fce299c9183438fb30745187628b
cassiemak <cassie.mak@gmail.com>
2015-01-23 13:36:08 +0800

    deleted mansonry js files and code and installed mansonry gem on rails

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,5 @@
 //= require angular-animate
 //= require angular-resource
 //= require angular-route
+//= require masonry/jquery.masonry
 //= require_tree .
--- app/assets/javascripts/masonry.js
+++
@@ -1,7 +0,0 @@
-var container = document.querySelector('#container');
-
-var msnry = new Masonry( container, {
-  // options...
-  itemSelector: '.item',
-  columnWidth: 200
-});
