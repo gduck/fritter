0591021847e5b11a5c6ca986d04b9341b16d5af0
cassiemak <cassie.mak@gmail.com>
2015-01-27 18:08:50 +0800

    added infinite scroll js stuff

--- app/assets/javascripts/masonry.js
+++ app/assets/javascripts/masonry.js
@@ -2,10 +2,36 @@ $(document).ready(function(){
   $('#masonry-container').masonry({
     itemSelector: '.box',
     columnWidth: 230,
+    // gutterWith: 40,
     isAnimated: !Modernizr.csstransitions
     // isRTL: true,
     // isFitWidth: true
-    });
+  });
+
   console.log('hello masonry');
   console.log('I see', $('.box').length, 'boxes');
+
+  // $container.infinitescroll({
+  //     // selector for the paged navigation
+  //     navSelector  : '#page-nav',
+  //     // selector for the NEXT link (to page 2)
+  //     nextSelector : '#page-nav a',
+  //     // selector for all items you'll retrieve
+  //     itemSelector : '.box',
+  //     loading: {
+  //         finishedMsg: 'No more pages to load.',
+  //         img: 'http://i.imgur.com/6RMhx.gif'
+  //     }
+  // },
+  // // trigger Masonry as a callback
+  // function(newElements){
+  //   // hide new items while they are loading
+  //   var $newElems = $( newElements ).css({ opacity: 0 });
+  //   // ensure that images load before adding to masonry layout
+  //   $newElems.imagesLoaded(function(){
+  //     // show elems now they're ready
+  //     $newElems.animate({ opacity: 1 });
+  //     $container.masonry( 'appended', $newElems, true );
+  //   });
+  // });
 });
