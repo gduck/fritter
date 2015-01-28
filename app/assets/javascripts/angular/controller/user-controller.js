8f385881d46adb034ec7ad5da697f8992cc09350
gduck <dale.github@thefoos.net>
2015-01-28 12:33:33 +0800

    sign-up page not connected to the controller

--- app/assets/javascripts/angular/controller/user-controller.js
+++ app/assets/javascripts/angular/controller/user-controller.js
@@ -1,5 +1,4 @@
-app.controller('UserCtrl', ['$scope', '$http',
-  function($scope, $http){
+app.controller('UserCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
 
   // $http.get('user.json').success(function(data){
   //   console.log('sucess on getting users');
@@ -20,10 +19,12 @@ app.controller('UserCtrl', ['$scope', '$http',
   })
 =======
 
+  console.log("here in the UserCtrl");
   $scope.user = UserServices;
 
   $http.get("/user/any").success(function(response,status){
-    UserServices.status = response.signedIn;
+    console.log("http request get user/any")
+    UserServices.logInStatus = response.signedIn;
   });
 
   $scope.userSignIn = function() {
@@ -33,13 +34,16 @@ app.controller('UserCtrl', ['$scope', '$http',
         'email': $scope.userEmail,
         'password': $scope.userPassword,
         'password_confirmation': $scope.userPassword
-      }
-    }
+      },
+      'commit': "Sign In"
+    };
     $http.post(url, data).success(function(response, status, xhr){
       console.log("response ", response);
+      UserServices.logInStatus = true;
     }).error(function(response) {
       console.log("problem!! - " + response);
-    })   
+      UserServices.logInStatus = false;
+    });
   }
 
   $scope.newUser = function() {
@@ -53,16 +57,19 @@ app.controller('UserCtrl', ['$scope', '$http',
       'commit': "Sign Up"
     };
     $http.post(url, data).success(function(response, status, xhr){
+      UserServices.logInStatus = true;
       console.log("SUCCESSFUL response ", response);
       console.log(status);
     }).error(function(response) {
       console.log("problem!! - " + response);
-    })
+      UserServices.logInStatus = false;
+    });
   }
 
   $scope.userSignOut = function() {
     $http.delete("/users/sign_out/").success(function(response,status){
-      UserServices.status = false;
+      UserServices.logInStatus = false;
+      console.log("ANYTHING DAMMIT");
     })
   }
 >>>>>>> added userServices service, creating a user object in which to store logged in status to share throughout the app
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,6 @@
 class UsersController < ApplicationController
+
+  def any
+    render json: {signedIn: user_signed_in?}
+  end  
 end
