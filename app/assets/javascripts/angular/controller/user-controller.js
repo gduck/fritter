756f301df87e46893fbfb4e4e287ece49923ba2e
cassiemak <cassie.mak@gmail.com>
2015-01-28 23:22:47 +0800

    After HP comments

--- app/assets/javascripts/angular/controller/user-controller.js
+++ app/assets/javascripts/angular/controller/user-controller.js
@@ -1,19 +1,18 @@
 app.controller('UserCtrl', ['$scope', '$http',
   function($scope, $http){
 
-
   // $http.get('user.json').success(function(data){
   //   console.log('sucess on getting users');
   //   console.log(data);
   //   $scope.user = data.user;
   // })
   
-    $http.get('pins.json').success(function(data){
+  $http.get('pins.json').success(function(data){
     console.log('success on getting pins from UserCtrl');
     $scope.pins = data.pins;
   })
 
-    $http.get('/categories.json').success(function(data){
+  $http.get('/categories.json').success(function(data){
     console.log('sucess on get categories');
     // console.log(data);
     $scope.categories = data.categories;
