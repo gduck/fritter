86599591f0857f998b94d7f7a7ecec2a1703f7ac
gduck <dale.github@thefoos.net>
2015-01-29 19:23:28 +0800

    starting search again

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -5,4 +5,12 @@ class Pin < ActiveRecord::Base
   validates :category_id, presence: true
 
   belongs_to :category
+
+  def self.search(search)
+    if search
+      joins(:pins).where('(pins.title.downcase || pins.optional_info.downcase) LIKE ?', "%#{search}%").includes(:pins).limit(50)
+    else
+      order(created_at: :desc).joins(:pins).includes(:pins).limit(50)
+    end
+  end
 end
