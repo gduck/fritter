3bbaca8c25e7b282980bab35b5f5ecb1c8fc5898
cassiemak <cassie.mak@gmail.com>
2015-02-02 13:18:30 +0800

    infinity scroll works but get requests continues even tho it has no more data/pins masonry doesn't work

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,9 +3,9 @@ class PinsController < ApplicationController
   def index
     if params[:keyword]||params[:category_id] 
       permitted_params = params.permit(:category_id)
-      @pins = Pin.includes(:category).where(permitted_params).where("title like '%#{params[:keyword]}%'").limit(50)
+      @pins = Pin.includes(:category).where(permitted_params).where("title like '%#{params[:keyword]}%'").limit(params[:limit])
     else
-      @pins = Pin.includes(:category).all
+      @pins = Pin.includes(:category).all.limit(params[:limit]).offset(params[:offset])
     end
 
   end
