cb0269e5bfd9c3b5c0417cd525b9bf524d2e7a11
Paul <paul.lugagnedelpon@gmail.com>
2015-02-03 01:46:46 +0800

    Fix Mansory and Infinite Scroll. Adjust PinController to accept single url.

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,11 +1,32 @@
 class PinsController < ApplicationController
 
   def index
-    if params[:keyword]||params[:category_id] 
+    # only category
+    if params[:keyword].blank? && !params[:category_id].blank?
       permitted_params = params.permit(:category_id)
-      @pins = Pin.includes(:category).where(permitted_params).where("title like '%#{params[:keyword]}%'").limit(params[:limit])
+      @pins = Pin.includes(:category)
+                 .where(permitted_params)
+                 .limit(params[:limit]).offset(params[:offset])
+
+    # only keyword
+    elsif params[:category_id].blank? && !params[:keyword].blank?
+      @pins = Pin.includes(:category)
+                 .where("title like '%#{params[:keyword]}%'")
+                 .limit(params[:limit]).offset(params[:offset])
+    
+    # both category and keyword
+    elsif !params[:keyword].blank? && !params[:category_id].blank?
+      permitted_params = params.permit(:category_id)
+      @pins = Pin.includes(:category)
+                 .where(permitted_params)
+                 .where("title like '%#{params[:keyword]}%'")
+                 .limit(params[:limit]).offset(params[:offset])
+
+    # No category and no keyword
     else
-      @pins = Pin.includes(:category).all.limit(params[:limit]).offset(params[:offset])
+      @pins = Pin.includes(:category)
+                 .all
+                 .limit(params[:limit]).offset(params[:offset])
     end
 
   end
