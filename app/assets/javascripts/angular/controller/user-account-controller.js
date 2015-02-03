5a4c5a4cc68d74fd5c082a7e3467e960637a9d4b
gduck <dale.github@thefoos.net>
2015-02-03 09:47:33 +0800

    need to reorganise files & folders to avoid controllers being called multiple times. Big reorganisation, new branch. closing this one, will revisit login issues after

--- app/assets/javascripts/angular/controller/user-account-controller.js
+++ app/assets/javascripts/angular/controller/user-account-controller.js
@@ -1,6 +1,6 @@
 app.controller('UserAccCtrl', ['$scope', '$http', '$location', 'UserServices',
   function($scope, $http, $location, UserServices){
-
+    console.log('Im the UserAccCtrl!');
   $scope.user = UserServices;
 
 
