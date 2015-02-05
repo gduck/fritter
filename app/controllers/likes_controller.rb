c94d053e51272a5670faf6b5ef8e05545876f30d
gduck <dale.github@thefoos.net>
2015-02-05 14:34:16 +0800

    search in likes implemented

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,8 +2,13 @@ class LikesController < ApplicationController
 
  def index
     # if current_user
-      @pins = current_user.pins.includes(:category).limit(params[:limit]).offset(params[:offset])
+    if params[:keyword]||params[:category_id] 
+      permitted_params = params.permit(:category_id)
+      @pins = current_user.pins.includes(:category).where(permitted_params).where("title LIKE '%#{params[:keyword]}%'").order('created_at DESC').limit(params[:limit]).offset(params[:offset])
       puts ">>>>>>>>>>>>>>>>>>>> @pins " + @pins.to_s
+    else
+      @pins = current_user.pins.includes(:category).order('created_at DESC').limit(params[:limit]).offset(params[:offset])
+    end
       # @likes = current_user.likes.includes(:pin).limit(params[:limit]).offset(params[:offset])
     # else
       # @pins = nil
