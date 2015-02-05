ea04f75d07e5b27e4c24bec68c785a7f25aeb507
gduck <dale.github@thefoos.net>
2015-02-05 14:25:58 +0800

    search and categories working with no repeats in infinite scroll and next page

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -3,7 +3,7 @@ class PinsController < ApplicationController
   def index
     if params[:keyword]||params[:category_id] 
       permitted_params = params.permit(:category_id)
-      @pins = Pin.includes(:category).where(permitted_params).where("title like '%#{params[:keyword]}%'").order('created_at DESC').limit(params[:limit]).offset(params[:offset])
+      @pins = Pin.includes(:category).where(permitted_params).where("title LIKE '%#{params[:keyword]}%'").order('created_at DESC').limit(params[:limit]).offset(params[:offset])
     else
       @pins = Pin.includes(:category).all.order('created_at DESC').limit(params[:limit]).offset(params[:offset])
     end
@@ -11,6 +11,11 @@ class PinsController < ApplicationController
 
   def show
     @pin = Pin.find(params[:id])
+  end
+
+  protected
+  def permitted_params 
+    params.require(:pin).permit(:keyword, :category_id, :limit, :offset)
   end
   
 end
