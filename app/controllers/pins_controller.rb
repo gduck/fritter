6b75e3edc6073b485ddbaee69f6319adc776d838
gduck <dale.github@thefoos.net>
2015-02-05 18:15:48 +0800

    clean likePinToggle

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,7 +1,7 @@
 class PinsController < ApplicationController
 
   def index
-    if params[:keyword]||params[:category_id] 
+    if params[:keyword] || params[:category_id] 
       permitted_params = params.permit(:category_id)
       @pins = Pin.includes(:category).where(permitted_params).where("title LIKE '%#{params[:keyword]}%'").order('created_at DESC').limit(params[:limit]).offset(params[:offset])
     else
