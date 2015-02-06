6c1ab8385d25cd53160287707c85b5afe105d523
gduck <dale.github@thefoos.net>
2015-02-06 12:38:02 +0800

    removed comment

--- app/assets/javascripts/angular/controller/user-account-controller.js
+++ app/assets/javascripts/angular/controller/user-account-controller.js
@@ -8,20 +8,7 @@ app.controller('UserAccCtrl', ['$scope', '$rootScope', '$http', '$location', 'Us
     console.log("In watch function header controller, args: ", args);
     $scope.user = args;
   });
-
-  $scope.getUserDetails = function() {
-    $http.get("/user/get.json").success(function(response,status){
-      console.log('i receive this response: ',response);
-      UserServices.signedIn = response.signedIn;
-      UserServices.id = response.id;
-      UserServices.username = response.username;
-      UserServices.userprofile = response.userprofile;
-      UserServices.email = response.email;
-      console.log("in get user details, UserServices: ", UserServices);
-      // console.log("the user services response is ", response);
-    });
-  }
-
+  
   $scope.userSignIn = function() {
     $scope.user.signIn($scope.signinEmail, $scope.signinPassword);
   }
